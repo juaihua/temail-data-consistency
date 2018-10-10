9e5ae65ae6763d94cf9b129619a805a3680e9339
王选中 <wangxuanzhong@syswin.com>
2018-10-10 16:24:31 +0800

    project version 1.0.0

--- consistency-spring/src/main/resources/sql/schema.sql
+++ consistency-spring/src/main/resources/sql/schema.sql
@@ -3,8 +3,9 @@ CREATE TABLE `listener_event` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `status` varchar(10) NOT NULL DEFAULT 'new' COMMENT '监听事件类型new pending complete',
   `content_id` bigint(20) NOT NULL COMMENT '监听同步消息id',
-  `content` varchar(1000) DEFAULT NULL COMMENT '同步内容',
+  `content` mediumtext DEFAULT NULL COMMENT '消息内容',
   `insert_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '生成时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
   `from_addr` varchar(100) DEFAULT NULL,
   `to_addr` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
