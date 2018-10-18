1b7db3653d5e106f4c64a96f95da24c9faef58c6
景家栋 <jingjiadong@syswin.com>
2018-10-18 14:42:52 +0800

    ZJCB-jingjiadong integrate disconf

--- consistency-spring/src/main/java/com/syswin/temail/data/consistency/domain/ListenerEvent.java
+++ consistency-spring/src/main/java/com/syswin/temail/data/consistency/domain/ListenerEvent.java
@@ -4,15 +4,22 @@ import java.sql.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class ListenerEvent {
   private long id;
+  @NonNull
   private SendingStatus status;
+  @NonNull
   private String content;
+  @NonNull
   private String topic;
+  @NonNull
   private String tag;
   private Timestamp insertTime;
   private Timestamp updateTime;
