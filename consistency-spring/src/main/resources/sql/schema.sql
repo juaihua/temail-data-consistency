b6fbbb2d2f9b7170912b342d46bb1ae887b72f9a
景家栋 <jingjiadong@syswin.com>
2018-10-31 17:57:15 +0800

    ZJCB-jingjiadong update sql schema

--- consistency-spring/src/main/resources/sql/schema.sql
+++ consistency-spring/src/main/resources/sql/schema.sql
@@ -2,9 +2,9 @@
 CREATE TABLE `listener_event` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `status` varchar(10) NOT NULL DEFAULT 'new' COMMENT '监听事件类型NEW SENDED',
-  `content` mediumtext DEFAULT NULL COMMENT '消息内容',
-  `topic` varchar(100) DEFAULT NULL,
-  `tag` varchar(32) DEFAULT NULL,
+  `content` mediumtext DEFAULT '' COMMENT '消息内容',
+  `topic` varchar(100) DEFAULT '',
+  `tag` varchar(32) DEFAULT '',
   `insert_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '生成时间',
   `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`id`)
