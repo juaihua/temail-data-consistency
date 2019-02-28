7fc5028e3e0f407b1da1298e82705b68befcb58a
殷湘 <yinxiang@syswin.com>
2019-02-28 11:53:22 +0800

    separate library modules into dedicated code base

--- database-event-stream/src/main/java/com/syswin/library/database/event/stream/DbEventStreamConnectionException.java
+++
@@ -1,8 +0,0 @@
-package com.syswin.library.database.event.stream;
-
-public class DbEventStreamConnectionException extends DbEventStreamException {
-
-  public DbEventStreamConnectionException(String message, Throwable e) {
-    super(message, e);
-  }
-}
--- stateful-task-runner/src/main/java/com/syswin/library/stateful/task/runner/zookeeper/ZookeeperPaths.java
+++
@@ -1,6 +0,0 @@
-package com.syswin.library.stateful.task.runner.zookeeper;
-
-class ZookeeperPaths {
-
-  static final String ZK_ROOT_PATH = "/syswin";
-}
